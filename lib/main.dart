f53c3e860a3616a879de60cfa68cfec12c119bc8
Mahmoud abdelwahab <mahmoud.elziany@gmail.com>
2024-11-13 18:07:28 +0200

    setup project

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
+import 'package:bloc_breaking/app_router.dart';
+
 void main() {
-  runApp(const BreakingBadApp());
+  runApp(BreakingBadApp(appRouter: AppRouter(),));
 }
 
 class BreakingBadApp extends StatelessWidget {
-  const BreakingBadApp({super.key});
+  
+final AppRouter appRouter;
 
+  const BreakingBadApp({super.key, required this.appRouter});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
     debugShowCheckedModeBanner: false,
-    onGenerateRoute: ,
+    onGenerateRoute: appRouter.generateRoute,
     );
   }
 }
